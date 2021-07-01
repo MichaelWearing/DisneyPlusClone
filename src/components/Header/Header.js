9b74287b37cc643ea3db172e18cb3df0a85ed8d2
Mikey <mikeywearing@gmail.com>
2021-07-01 14:30:48 +0200

    Added the rest of the options to the Header started working on Login with Friebase

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 // Styles
-import { Nav, Logo, NavMenu } from "./Header.styles";
+import { Nav, Logo, NavMenu, Login } from "./Header.styles";
 
 export default function Header() {
   return (
@@ -9,11 +9,32 @@ export default function Header() {
         <img src="/images/logo.svg" alt="Disney+" />
       </Logo>
       <NavMenu>
-        <a href="/home">
-          <img src="/images/home-icon.svg" alt="HOME" />
-          <span>HOME</span>
-        </a>
-      </NavMenu>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="HOME" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="SEARCH" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="ORIGINALS" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="MOVIES" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="SERIES" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+          <Login>Login</Login>
     </Nav>
   );
 }
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -79,7 +79,7 @@ export const NavMenu = styled.div`
         width: auto;
       }
     }
-    
+
     &:hover {
       span:before {
         transform: scaleX(1);
@@ -91,4 +91,19 @@ export const NavMenu = styled.div`
   /* @media (max-width: 768px) {
   display: none;
 } */
+`;
+
+export const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
 `;
